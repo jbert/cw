fcfe50f494d8338a345b68488e4f2c3f841e0fc2
John Berthels <john.berthels@gmail.com>
2025-10-05 14:41:27 +0100

    draw text

--- app/CW/SDL.hs
+++ app/CW/SDL.hs
@@ -6,11 +6,14 @@ import Control.Concurrent
 import Control.Monad
 import qualified Data.Maybe as Maybe
 import Data.Text (Text)
+import qualified Data.Text as Text
 import Data.Word
 import Foreign.C.Types
 import qualified SDL
+import qualified SDL.Font as TTF
 
 import CW.Scene (Scene)
+import qualified CW.Scene as Scene
 
 data Config = Config
     { width :: Int
@@ -18,12 +21,13 @@ data Config = Config
     , name :: Text
     , bgColour :: SDL.V4 Word8
     , fgColour :: SDL.V4 Word8
+    , font :: TTF.Font
     }
 
 data Pt = Pt Double Double
 
-toSDL :: Config -> Pt -> SDL.Point SDL.V2 CInt
-toSDL conf (Pt px py) = SDL.P $ SDL.V2 (fromIntegral rx) (fromIntegral ry)
+toSDLV2 :: Config -> Pt -> SDL.V2 CInt
+toSDLV2 conf (Pt px py) = SDL.V2 (fromIntegral rx) (fromIntegral ry)
   where
     w = width conf
     h = height conf
@@ -32,9 +36,15 @@ toSDL conf (Pt px py) = SDL.P $ SDL.V2 (fromIntegral rx) (fromIntegral ry)
     rx = round x :: Integer
     ry = round y :: Integer
 
+toSDL :: Config -> Pt -> SDL.Point SDL.V2 CInt
+toSDL conf p = SDL.P $ toSDLV2 conf p
+
 sdlMain :: Chan (Maybe Scene) -> IO ()
 sdlMain sceneChan = do
     SDL.initializeAll
+    TTF.initialize
+
+    fnt <- TTF.load "DejaVuSans.ttf" 48
 
     let conf =
             Config
@@ -43,6 +53,7 @@ sdlMain sceneChan = do
                 , name = "CW"
                 , bgColour = SDL.V4 0 0 255 255
                 , fgColour = SDL.V4 0 255 0 0
+                , font = fnt
                 }
 
     window <-
@@ -51,6 +62,7 @@ sdlMain sceneChan = do
             SDL.defaultWindow{SDL.windowInitialSize = SDL.V2 (fromIntegral $ width conf) (fromIntegral $ height conf)}
     renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
     sdlLoop conf renderer sceneChan
+    TTF.free fnt
     SDL.destroyWindow window
 
 drawMaybeScene :: Config -> SDL.Renderer -> Maybe Scene -> IO ()
@@ -86,5 +98,13 @@ drawLine conf renderer (p1, p2) = do
 
 drawScene :: Config -> SDL.Renderer -> Scene -> IO ()
 drawScene conf renderer scene = do
-    let lines = [(Pt 0.1 0.1, Pt 0.8 0.8)]
-    mapM_ (drawLine conf renderer) lines
+    let ls = [(Pt 0.1 0.1, Pt 0.8 0.8)]
+    mapM_ (drawLine conf renderer) ls
+
+    surface <- TTF.solid (font conf) (fgColour conf) $ Text.pack $ show (Scene.ticks scene)
+    texture <- SDL.createTextureFromSurface renderer surface
+    let bl = toSDL conf (Pt 0.1 0.1)
+    let tr = toSDLV2 conf (Pt 0.4 0.1)
+    let dstRect = SDL.Rectangle bl tr
+    SDL.copy renderer texture Nothing (Just dstRect)
+    SDL.freeSurface surface
